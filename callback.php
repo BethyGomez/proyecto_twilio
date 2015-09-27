df8d9af82489268452a6867a294093142cfb98f9
BethyGomez <betygomez18@gmail.com>
2015-09-26 20:59:37 -0500

    Create callback.php

---
+++ callback.php
@@ -0,0 +1,48 @@
+<?php
+header("content-type: text/xml");
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+?>
+<Response>
+<Say>A customer at the number <?php echo
+$_REQUEST['number']?> is calling</Say>
+<Dial record=true><?php echo $_REQUEST['number']?></Dial>
+</Response>
+Did you catch what we did here? We told the Dial command to record the call. This
+means anything that is spoken during this call is now recorded.
+7. Finally, upload a file named recording.php to your website:
+<?php
+if( isset($_GET['csid']) ){
+getRecording( $_GET['csid'] );
+}else{
+die( "Invalid recording!");
+}
+function getRecording($caSID){
+global $accountsid,$authtoken;
+$version = '2010-04-01';
+$url = "https://api.twilio.com/2010-04-
+01/Accounts/{$accountsid}/
+Calls/{$caSID}/Recordings.xml";
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_USERPWD,
+"{$accountsid}:{$authtoken}");
+curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+$output = curl_exec($ch);
+$info = curl_getinfo($ch);
+curl_close($ch);
+$output = simplexml_load_string($output);
+echo "<table>";
+foreach ($output->Recordings->Recording as $recording)
+{
+echo "<tr>";
+echo "<td>".$recording->Duration." seconds</td>";
+echo "<td>".$recording->DateCreated."</td>";
+echo '<td><audio src="https://api.twilio.com/
+2010-04-01/Accounts/'.$sid.'/Recordings/
+'.$recording->Sid.'.mp3" controls preload="auto"
+autobuffer></audio></td>';
+echo "</tr>";
+}
+echo "</table>";
+}
