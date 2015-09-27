60a829de77afa435007492f8632fcb494986b6a1
BethyGomez <betygomez18@gmail.com>
2015-09-26 19:17:03 -0500

    Update factores-voice.php

--- factores-voice.php
+++ factores-voice.php
@@ -20,3 +20,38 @@ header("Location: two-factor-voice.php?message=" .
 urlencode($message));
 exit;
 }
+
+?>
+<html>
+<body>
+<p>Please enter a username, and a phone number you can be
+reached at, we will then call you with your one-time
+password</p>
+<span id="message">
+<?php
+echo cleanVar('message');
+$action = (isset($_SESSION['password'])) ? 'login' :
+'token';
+?>
+</span>
+<form id="reset-form" method="POST" class="center">
+<input type="hidden" name="action" value="<?php echo
+$action;
+?>" />
+<p>Username: <input type="text" name="username"
+id="username"
+value="<?php echo $_SESSION['username']; ?>" /></p>
+<?php if (isset($_SESSION['password'])) { ?>
+<p>Password: <input type="password" name="password"
+id="password" /></p>
+<?php } else { ?>
+<p>Phone Number: <input type="text" name="phone_number"
+id="phone_number" /></p>
+<input type="hidden" name="method" value="voice" />
+<?php } ?>
+<p><input type="submit" name="submit" id="submit"
+value="login!"/></p>
+<p>&nbsp;</p>
+</form>
+</body>
+</html>
